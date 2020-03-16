bb20aad7076ac57aac796c77e449b3644379e0bf
Aravindsagar P A <52458478+aravindsagarpa100@users.noreply.github.com>
2020-03-16 22:34:09 +0530

    Add files via upload

---
+++ Tea/Read.java
@@ -0,0 +1,8 @@
+public class Read
+{
+public static void main(String args[])
+{
+   Tea s=new Tea();
+   s.D();
+}
+}
---
+++ Tea/Tea.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class Tea here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Tea
+{
+    void D()
+    {
+    System.out.println("Hello");
+    }
+}
